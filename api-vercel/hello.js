5c4a3ae058681f1f30a58e8df329188a0ae1e77e
MarinaWyss <marina.bennett.wyss@gmail.com>
2025-06-12 16:58:46 -0700

    different file extensions will surely fix it

--- api-vercel/hello.js
+++
@@ -1,7 +0,0 @@
-// Simple test API function for Vercel
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: 'Hello from Vercel serverless function!',
-    success: true
-  });
-}; 
